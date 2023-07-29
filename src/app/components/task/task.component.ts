ef51ef942794cd87a861e7450255ee8a82c53d60
Nandansanapala <99603267+Nandansanapala@users.noreply.github.com>
2023-07-29 22:20:50 +0530

    Add files via upload

---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TaskService } from 'src/app/services/task.service';
+import { Observable } from 'rxjs';
+
+
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent {
+  tasks: Task[] = [];
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter(
+          (t) => t.id !== task.id)))
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe()
+  }
+  addTask(task : Task) {
+    this.taskService.addTask(task).subscribe((tasks) => (this.tasks.push(task)));
+  }
+    
+  
+
+}
